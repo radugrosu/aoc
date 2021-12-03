aec7d236fbb22c6ba8a05481b7a4bbba886948c8
radu <radu.grosu@gmail.com>
2021-12-03 11:03:39 +0000

    puzzle 2

--- 2021/2.py
+++ 2021/2.py
@@ -1,41 +1,64 @@
-# https://adventofcode.com/2021/day/1
-from typing import Sequence
+# https://adventofcode.com/2021/day/2
+from typing import Sequence, List
 from util import read_input
 
 test_inputs = """
-199
-200
-208
-210
-200
-207
-240
-269
-260
-263""".strip().splitlines()
-
-test_inputs = [int(item.strip()) for item in test_inputs]
-inputs = [int(item) for item in read_input("./inputs/1.txt")]
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+""".strip().splitlines()
 
 
-def part_1(inputs: Sequence[int]) -> int:
-    num_larger = 0
-    for i in range(1, len(inputs)):
-        if inputs[i] > inputs[i - 1]:
-            num_larger += 1
-    return num_larger
+def process_inputs(inputs: Sequence[str]) -> Sequence[List]:
+    out = []
+    for line in inputs:
+        dir, value = line.strip().split()
+        out.append([dir, int(value)])
+    return out
 
 
-assert part_1(test_inputs) == 7
+test_inputs = process_inputs(test_inputs)
 
 
-def part_2(inputs: Sequence[int]) -> int:
-    """3-sized window"""
-    num_larger = 0
-    for i in range(4, len(inputs) + 1):
-        if sum(inputs[i - 4 : i - 1]) < sum(inputs[i - 3 : i]):
-            num_larger += 1
-    return num_larger
+def part_1(inputs: Sequence[int]) -> Sequence[int]:
+    pos = [0, 0]
+    for dir, value in inputs:
+        if dir == "forward":
+            index = 0
+        elif dir == "down":
+            index = 1
+        elif dir == "up":
+            index = 1
+            value *= -1
+        else:
+            raise ValueError(f"Unexpected direction {dir}")
+        pos[index] += value
+    return pos
 
 
-assert part_2(test_inputs) == 5
+assert part_1(test_inputs) == [15, 10]
+
+inputs = process_inputs(read_input("./inputs/2.txt"))
+print(part_1(inputs))
+
+
+def part_2(inputs: Sequence[int]) -> Sequence[int]:
+    pos = [0, 0]  # horizontal, depth
+    aim = 0
+    for dir, value in inputs:
+        if dir == "down":
+            aim += value
+            continue
+        if dir == "up":
+            aim -= value
+            continue
+        pos[0] += value
+        pos[1] += aim * value
+    return pos
+
+
+assert part_2(test_inputs) == [15, 60]
+print(part_2(inputs))
